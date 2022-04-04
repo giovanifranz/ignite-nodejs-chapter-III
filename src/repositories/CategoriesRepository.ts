7acaea0bee0c23b25a55cbc6aa8d7e34a81c5dcd
giovanifranz <giovanifranz@gmail.com>
2022-04-04 00:53:01 -0300

    feat: :sparkles: Criando API com NodeJs

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,34 @@
+import { Category } from '../model/Category'
+
+interface ICreateCategoryDTO {
+  name: string
+  description: string
+}
+
+export class CategoriesRepository {
+  private categories: Category[]
+
+  constructor() {
+    this.categories = []
+  }
+
+  create({ description, name }: ICreateCategoryDTO): void {
+    const category = new Category()
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date()
+    })
+
+    this.categories.push(category)
+  }
+
+  list(): Category[] {
+    return this.categories
+  }
+
+  findByName(name: string): Category | undefined {
+    return this.categories.find((category) => category.name === name)
+  }
+}
