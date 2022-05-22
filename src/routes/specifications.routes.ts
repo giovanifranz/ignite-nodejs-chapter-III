253ce1c7b7a20f956df4be457c502e7401fc70d5
giovanifranz <giovanifranz151@gmail.com>
2022-05-22 17:33:56 -0300

    refactor: :recycle: Criando use case de especificação

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,15 +1,9 @@
 import { Request, Response, Router } from 'express'
 
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationRepository'
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService'
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification'
 
 export const specificationRoutes = Router()
-const specificationsRepository = new SpecificationsRepository()
+
 specificationRoutes.post('/', (request: Request, response: Response) => {
-  const { name, description } = request.body
-
-  const createSpecificationService = new CreateSpecificationService(specificationsRepository)
-  createSpecificationService.execute({ name, description })
-
-  return response.status(201).send()
+  return createSpecificationController.handle(request, response)
 })
