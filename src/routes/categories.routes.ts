93ddbbd139966dbdd9318315e290bdc8add5be6e
giovanifranz <giovanifranz151@gmail.com>
2022-05-22 18:02:40 -0300

    feat: :sparkles: Criando upload de arquivos (Multer)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,15 @@
 import type { Request, Response } from 'express'
 import { Router } from 'express'
+import multer from 'multer'
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 export const categoriesRoutes = Router()
+
+const upload = multer({
+  dest: './tmp/',
+})
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   return createCategoryController.handle(request, response)
@@ -12,4 +17,10 @@ categoriesRoutes.post('/', (request: Request, response: Response) => {
 
 categoriesRoutes.get('/', (request: Request, response: Response) => {
   return listCategoriesController.handler(request, response)
+})
+
+categoriesRoutes.post('/import', upload.single('file'), (request: Request, response: Response) => {
+  const { file } = request
+  console.log(file)
+  return response.send()
 })
