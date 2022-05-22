1e40e2d096e2f47002125022982fb8b4e46e1f7a
giovanifranz <giovanifranz151@gmail.com>
2022-05-22 19:29:13 -0300

    feat: :sparkles: Inserindo dados do upload no repositorio

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,46 @@
+import csvParser from 'csv-parser'
+import fs from 'node:fs'
+
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+
+interface IImportCategory {
+  name: string
+  description: string
+}
+
 export class ImportCategoryUseCase {
-  execute(file: Express.Multer.File) {
-    console.log(file)
+  constructor(private categoryRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = []
+      const parseFile = csvParser(['name', 'description'])
+
+      fs.createReadStream(file.path).pipe(parseFile)
+
+      parseFile
+        .on('data', async (line: IImportCategory) => {
+          categories.push(line)
+        })
+        .on('end', () => {
+          resolve(categories)
+        })
+        .on('error', (error) => {
+          reject(error)
+        })
+    })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    categories.map((category) => {
+      const { name, description } = category
+
+      const existCategoryInRepository = this.categoryRepository.findByName(name)
+
+      if (!existCategoryInRepository) {
+        this.categoryRepository.create({ name, description })
+      }
+    })
   }
 }
